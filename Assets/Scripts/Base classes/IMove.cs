b0954661588aeec3c12f728f280e68cfe835d7a0
Luis Felipe Velasquez Garcia <pipevelasquez7@gmail.com>
2023-11-20 20:52:49 -0500

    Added Base class for enemies and added scripts for each of them that derive from the base class.

---
+++ Assets/Scripts/Base classes/IMove.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public interface IMove
+{
+    void Move(float2 direction);
+}
---
+++ Assets/Scripts/Enemies/Boceto de papel/BocetoDePapel.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class BocetoDePapel : EnemyBase
+{
+
+    // Start is called before the first frame update
+    void Start ()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+    }
+
+    public override void Attack ()
+    {
+      
+    }
+
+    public override void Die ()
+    {
+       
+    }
+
+    public override void Move (float2 position)
+    {
+      
+    }
+}
